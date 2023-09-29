a4b694d02b8f82dce51c6ad671903e30f7d59f9f
Casey James <caseyjamesmcardle@gmail.com>
2023-09-29 14:36:02 +0100

    Reordered sections for responsive design.
    On smaller screens, the 'key' section now appears above the 'info' section. This order swaps on larger screens for optimal layout.

--- src/app/landingpage/Info.tsx
+++ src/app/landingpage/Info.tsx
@@ -39,6 +39,30 @@ const Info: React.FC = () => {
 
   return (
     <div className="mt-0 md:mt-10 grid grid-cols-1 lg:grid-cols-2 gap-10 md:gap-20">
+      {/* Key Section */}
+      <div id="key" className="order-first lg:order-last">
+        <h3 className="accent h3 font-semibold">Key</h3>
+        {airQualityLevels.map((level) => (
+          <div key={level.color} className="flex items-center my-4">
+            <div className="relative w-10 h-10 flex-shrink-0 flex-grow-0 mr-4">
+              <svg
+                width="100%"
+                height="100%"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="16" cy="16" r="15" fill={level.color}></circle>
+              </svg>
+            </div>
+            <div className="flex-grow">
+              <span className="font-bold mr-2">{level.name}:</span>
+              <span>{level.description}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      
+      {/* Info Section */}
       <div id="info">
         <h2 className="text-start accent text-lg xl:text-xl font-semibold mb-4">
           Understanding Air Quality with AirActive
@@ -63,30 +87,9 @@ const Info: React.FC = () => {
           Sign Up
         </button>
       </div>
-
-      <div id ="key">
-        <h3 className="accent h3 font-semibold">Key</h3>
-        {airQualityLevels.map((level) => (
-          <div key={level.color} className="flex items-center my-4">
-            <div className="relative w-10 h-10 flex-shrink-0 flex-grow-0 mr-4">
-              <svg
-                width="100%"
-                height="100%"
-                viewBox="0 0 32 32"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle cx="16" cy="16" r="15" fill={level.color}></circle>
-              </svg>
-            </div>
-            <div className="flex-grow">
-              <span className="font-bold mr-2">{level.name}:</span>
-              <span>{level.description}</span>
-            </div>
-          </div>
-        ))}
-      </div>
     </div>
   );
-};
-
+  
+        }
+        
 export default Info;
