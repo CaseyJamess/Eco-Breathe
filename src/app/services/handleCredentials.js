0c1a97c7269ffad5cd6ee3a5192d065eccd8af8f
Casey James <caseyjamesmcardle@gmail.com>
2023-10-11 13:51:41 +0100

    Initial API endpoint for signup created
    Created API endpoint to send user sign up data to the server, needs further error handling and notification

--- src/app/services/handleCredentials.js
+++ src/app/services/handleCredentials.js
@@ -7,15 +7,14 @@ export const handleRegistrationData = async (signUpData) => {
     console.log('Attempting to send login credentials to back end');
     console.log('Login credentials are: ', signUpData);
 
-    try{
-        const data = {}
-        const response = axios.post(`${Server_URL}/auth/v1/store_credentials`, data);
+    try {
 
+        const response = await axios.post(`${Server_URL}/auth/v1/store_credentials`, signUpData);
+        console.log('this is the response data : ', response.data);
+        return response.data;
     }
-
-    catch{
-
+    catch (error) {
+        console.log('Error sending sign up data', error);
+        return { error: "Failed to send sign up data to server" };
     }
-
-
 }
